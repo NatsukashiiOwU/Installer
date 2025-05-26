308756f049be890df1500c0a71a6cd875d09b6c8
kita <alpsdi77@gmail.com>
2025-05-26 16:11:05 +0400

    Update constants.go

--- constants.go
+++ constants.go
@@ -7,39 +7,39 @@
 package main
 
 import (
-	"image/color"
-	"vencordinstaller/buildinfo"
+    "image/color"
+    "vencordinstaller/buildinfo"
 )
 
-const ReleaseUrl = "https://api.github.com/repos/Vendicated/Vencord/releases/latest"
-const ReleaseUrlFallback = "https://vencord.dev/releases/vencord"
+const ReleaseUrl = "https://api.github.com/repos/NatsukashiiOwU/Vencord/releases/tags/natsukashii-vencord-dev"
+const ReleaseUrlFallback = "https://github.com/NatsukashiiOwU/Vencord/releases/download/natsukashii-vencord-dev/Vencord.js"
 const InstallerReleaseUrl = "https://api.github.com/repos/Vencord/Installer/releases/latest"
 const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
 
-var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/Vencord/Installer)"
+var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/NatsukashiiOwU/Installer)"
 
 var (
-	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
-	DiscordRed    = color.RGBA{R: 0xEC, G: 0x41, B: 0x44, A: 0xFF}
-	DiscordBlue   = color.RGBA{R: 0x58, G: 0x65, B: 0xF2, A: 0xFF}
-	DiscordYellow = color.RGBA{R: 0xfe, G: 0xe7, B: 0x5c, A: 0xff}
+    DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
+    DiscordRed    = color.RGBA{R: 0xEC, G: 0x41, B: 0x44, A: 0xFF}
+    DiscordBlue   = color.RGBA{R: 0x58, G: 0x65, B: 0xF2, A: 0xFF}
+    DiscordYellow = color.RGBA{R: 0xfe, G: 0xe7, B: 0x5c, A: 0xff}
 )
 
 var LinuxDiscordNames = []string{
-	"Discord",
-	"DiscordPTB",
-	"DiscordCanary",
-	"DiscordDevelopment",
-	"discord",
-	"discordptb",
-	"discordcanary",
-	"discorddevelopment",
-	"discord-ptb",
-	"discord-canary",
-	"discord-development",
-	// Flatpak
-	"com.discordapp.Discord",
-	"com.discordapp.DiscordPTB",
-	"com.discordapp.DiscordCanary",
-	"com.discordapp.DiscordDevelopment",
+    "Discord",
+    "DiscordPTB",
+    "DiscordCanary",
+    "DiscordDevelopment",
+    "discord",
+    "discordptb",
+    "discordcanary",
+    "discorddevelopment",
+    "discord-ptb",
+    "discord-canary",
+    "discord-development",
+    // Flatpak
+    "com.discordapp.Discord",
+    "com.discordapp.DiscordPTB",
+    "com.discordapp.DiscordCanary",
+    "com.discordapp.DiscordDevelopment",
 }
